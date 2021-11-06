b46eafd65e9d9434879b604ce3a8922eb2383957
F-B-Lami <fonyuyborislami@gmail.com>
2021-11-06 15:31:48 +0200

    added album  and track interfaces and updated corresponding service and component

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,9 @@
 import {Http, Response} from '@angular/http';
 import { Injectable } from '@angular/core';
-import { Observable } from "rxjs";
+import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map';
 import {map} from 'rxjs/operators';
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
@@ -14,8 +15,8 @@ export class ProductService {
 
   // backend CRUD
   // R
-  getAlbum(id: number){
-    return this._http.get(this._albumUrl).pipe(map(response => response.json()));
+  getAlbum(id: number): Observable<Album>{
+    return this._http.get(this._albumUrl).pipe(map(response => <Album>response.json()));
   }
 
 }
