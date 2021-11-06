a2dcb056f324fa5cf4922645b9b8953af47f0a50
F-B-Lami <fonyuyborislami@gmail.com>
2021-11-06 16:10:05 +0200

    bug fix part 4

--- src/spec/mocha-specs/part4/album-interface-has-four-properties.mocha.spec.js
+++ src/spec/mocha-specs/part4/album-interface-has-four-properties.mocha.spec.js
@@ -12,7 +12,7 @@ describe('Album Interface', function () {
     }
     let re = /export\s+interface\s+Album\s*\{\s*([\w\s\:\;\[\]]+)\s*\}/
     let match = file.match(re);
-    assert(Array.isArray(match) && match != null, "There's an `album.ts` file, but it doesn't export an interface named `Album`.");
+    assert(Array.isArray(match) && match != null, "There's an `album.ts` file, but it doesn't export an interface named `Album`." + match);
     
     let arr = match[1].split('\n');
     for (let i = 0; i < arr.length; i++) {
