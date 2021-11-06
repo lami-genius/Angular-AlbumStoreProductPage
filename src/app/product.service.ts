cdfb0a8aeef4a9cf12155310beade5f07eb11054
F-B-Lami <fonyuyborislami@gmail.com>
2021-11-06 17:54:22 +0200

    part 6 completed and tested locally :-)

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,12 +4,14 @@ import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map';
 import {map} from 'rxjs/operators';
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   // class attributes
-  private _albumUrl: string = '../assets/album.json';    // data repo
+  private _albumUrl: string = '../assets/album.json';    // assets data repo
+  private _productsUrl: string = '../assets/products.json';    // products data repo
 
   constructor(private _http: Http) { }
 
@@ -17,6 +19,10 @@ export class ProductService {
   // R
   getAlbum(id: number): Observable<Album>{
     return this._http.get(this._albumUrl).map(response => <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
   }
 
 }
